5494ed48e69880654380944917e9d6c73e7ff9d7
Mangoberrymelon <wonwoopoo@gmail.com>
2023-05-26 23:42:52 +0900

    HW10

--- solveVelocityInPerifocalFrame.m
+++ solveVelocityInPerifocalFrame.m
@@ -1,10 +1,9 @@
 function velocityInPQW = solveVelocityInPerifocalFrame(semimajor_axis, eccentricity, true_anomaly)
-mu = 3.986004418*10^14;
+mu = 3.986004418*10^5;
 p = (semimajor_axis*(1-eccentricity^2));
 
-PQ_vel_P = sqrt(mu/p) * (-sin(true_anomaly));
-PQ_vel_Q = sqrt(mu/p) * (eccentricity+cos(true_anomaly));
+PQ_vel_P = sqrt(mu/p) * (-sin(deg2rad(true_anomaly)));
+PQ_vel_Q = sqrt(mu/p) * (eccentricity+cos(deg2rad(true_anomaly)));
 
 velocityInPQW = [PQ_vel_P, PQ_vel_Q 0]';
 end
-
