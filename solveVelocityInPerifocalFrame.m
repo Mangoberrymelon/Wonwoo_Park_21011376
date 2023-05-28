db05734a173d52c2846a8c1c7a8eb593ab7327fe
Mangoberrymelon <wonwoopoo@gmail.com>
2023-05-29 03:46:37 +0900

    .

--- solveVelocityInPerifocalFrame.m
+++ solveVelocityInPerifocalFrame.m
@@ -5,5 +5,5 @@ p = (semimajor_axis*(1-eccentricity^2));
 PQ_vel_P = sqrt(mu/p) * (-sin(deg2rad(true_anomaly)));
 PQ_vel_Q = sqrt(mu/p) * (eccentricity+cos(deg2rad(true_anomaly)));
 
-velocityInPQW = [PQ_vel_P, PQ_vel_Q 0]';
+velocityInPQW = [PQ_vel_P, PQ_vel_Q, 0]';
 end
