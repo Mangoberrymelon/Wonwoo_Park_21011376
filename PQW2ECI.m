8d010801ce5b0c410584b2a1e6d479324f4db259
Mangoberrymelon <wonwoopoo@gmail.com>
2023-06-01 23:38:40 +0900

    .

--- PQW2ECI.m
+++ PQW2ECI.m
@@ -1,7 +1,7 @@
 function rotation_matrix = PQW2ECI(arg_prg, inc_angle, RAAN)
 A = [cos(arg_prg), sin(arg_prg), 0; -sin(arg_prg), cos(arg_prg), 0; 0, 0, 1];
-B = [cos(inc_angle), 0, -sin(inc_angle); 0, 1, 0; sin(inc_angle), 0, cos(inc_angle)];
-C = [1, 0, 0; 0, cos(RAAN), sin(RAAN); 0, -sin(RAAN), cos(RAAN)];
+B = [1, 0, 0; 0, cos(inc_angle), sin(inc_angle); 0, -sin(inc_angle), cos(inc_angle)];
+C = [cos(RAAN), sin(RAAN), 0; -sin(RAAN), cos(RAAN), 0; 0, 0, 1];
 
 rotation_matrix = (A * B * C)';
 end
